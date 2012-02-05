aa4e8c94d197a6b323e58b3d3c8997f5d5ef319f
egor1989 <egor7ivanov@gmail.com>
2012-02-05 23:21:59 +0400

    port
    Signed-off-by: egor1989 <egor7ivanov@gmail.com>

--- app.js
+++ app.js
@@ -90,5 +90,5 @@ function getreport(url, report){
   });
 }
 
-app.listen(80);
-console.log("Express server listening on port %d in %s mode", app.address(), app.settings.env);
+app.listen(88);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
