02041f334c8c87cdef493820246c831bfbba5e54
egor1989 <egor7ivanov@gmail.com>
2012-02-06 14:21:35 +0400

    view
    Signed-off-by: egor1989 <egor7ivanov@gmail.com>

--- public/js/app.js
+++ public/js/app.js
@@ -35,66 +35,14 @@ function getRepo() {
                 $("#reportItem").text('Error: ' + r.error);
 				console.log('Error: ' + r.error);
             } else {
-				$("#reportItem").text(r.report);
-                console.log(r.report);
+				buidReport(r.report);
+				//console.log(r.report);
 				//playSong(url);
             }
         }
     });
 }
 
-function playSong(url) {
-    music = soundManager.createSound({
-        id: 'music',
-        url: url,
-        volume: 100,
-        autoLoad: true,
-        stream: false,
-        autoPlay: true,
-        whileplaying: whilePlaying,
-        onfinish: function() {
-            resetCanvas();
-        }
-    });
-}
-
-function whilePlaying() {
-    var now = this.position / 1000;
-
-    function randomColor() {
-        return "#"+((1<<24)*Math.random()|0).toString(16);
-    }
-   
-    while (samples.beats[next_beat][0] < now) {
-        var color = randomColor();
-        //for (var i = 0; i < 1; i++) {
-        //    drawRandomCircle(300, color);
-        //}
-        //fillBG(color);
-        drawRandomCircle(300, color);
-        next_beat++;
-    }
-    
-    var color = randomColor();
-    while (samples.onsets[next_onset][0] < now) {
-        //for (var i = 0; i < 5; i++) {
-        //    drawRandomCircle(100, color);
-        //}
-        drawRandomCircle(100, color);
-        next_onset++;
-    }
-}
-
-function drawRandomCircle(radius, color) {
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.arc(40 + 920*Math.random(), 40 + 520*Math.random(), radius, 0, Math.PI*2, true);
-    ctx.closePath();
-    ctx.fill();
-}
-
-function fillBG(color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(0, 0, 1000, 600);
-}
-
+function buildReport(report){
+  $("#reportItem").text(report);
+}              
